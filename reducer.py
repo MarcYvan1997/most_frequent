ad8618da5b9209ee707583c660f91fd36ad1a6ce
MarcYvan1997 <yvantchepgag@yahoo.fr>
2025-04-16 11:49:34 +0200

    Update reducer.py

--- reducer.py
+++ reducer.py
@@ -1,18 +1,18 @@
+# reducer
 import sys
 
-def read_kv_array(text):
-    key_values = {}
+max_word = None
+max_count = 0
 
-    for line in text:
-        key, value = line.strip().split('\t')
-        key_values[key] = key_values.get(key, 0) + int(value)
+for line in sys.stdin:
+    try:
+        _, word, count = line.strip().split('\t')
+        count = int(count)
+        if count > max_count:
+            max_count = count
+            max_word = word
+    except ValueError:
+        continue
 
-    return key_values
-
-if __name__ == '__main__':
-    word_counts = read_kv_array(sys.stdin)
-
-    # Find the word with the maximum count
-    most_frequent_word = max(word_counts, key=word_counts.get)
-    
-    print(f'Most frequent word: {most_frequent_word} ({word_counts[most_frequent_word]} times)')
+if max_word:
+    print(f"{max_word}\t{max_count}")
